8a14be64f65eb1a08ac9d591708539fbf2aa5647
Bartłomiej Ryba <bartlomiejrybaa@gmail.com>
2021-04-17 01:36:43 +0200

    switching yo sqlite

--- mobile-app/src/services/storage/db.ts
+++ mobile-app/src/services/storage/db.ts
@@ -1,9 +1,25 @@
-import Realm from 'realm';
+import * as SQLite from 'expo-sqlite';
+export const db = SQLite.openDatabase(`db.nospender`);
 
-const db = async (schema: Realm.ObjectSchema) => {
-
-    return await Realm.open({ schema: [schema] });
+export const initializeTransactionsTable = () => {
+   
+    db.transaction(tx => {
+        tx.executeSql(
+            `create table if not exists transactions 
+            (id integer, name VARCHAR, category INTEGER, amount DOUBLe,
+             insertTime DATETIME, longtitude DOUBLE, latitude DOUBLE);`)
+    });
 
 }
 
-export default db;
+
+export const initializeUserTable = () => {
+   
+    db.transaction(tx => {
+        tx.executeSql(
+            `create table if not exists transactions 
+            (id integer, name VARCHAR, category INTEGER, amount DOUBLe,
+             insertTime DATETIME, longtitude DOUBLE, latitude DOUBLE);`)
+    });
+
+}
