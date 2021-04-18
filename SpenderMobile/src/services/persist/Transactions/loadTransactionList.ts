1cec2495dba2cfd1ec5742099eefb02573ddb4e0
Bartłomiej Ryba <bartlomiejrybaa@gmail.com>
2021-04-18 10:58:46 +0200

    sqlite handled properly

--- SpenderMobile/src/services/persist/Transactions/loadTransactionList.ts
+++ SpenderMobile/src/services/persist/Transactions/loadTransactionList.ts
@@ -1,25 +1,24 @@
-// import { SQLError } from 'expo-sqlite';
 import { fetchTransactionList } from '../../../redux/actions/transactions';
-// import { db } from '../../storage/db';
+import { db } from '../../storage/db';
 
 export const loadTransactionList = async (userId: number) => {
    
     try {
                
-      //   db.transaction(tx => {
+        db.transaction(tx => {
 
-      //   tx.executeSql(`SELECT * FROM transactions WHERE userId = ?`, [userId],
-      //     (tx, results) => {
-      //       console.log(results.rows);
-      //       fetchTransactionList(results.rows)
-      //       // return results.rows;
-      //     });
-      // },
-      //   (error) => {
-      //     throw new Error(JSON.stringify(error));
-      //   }
+        tx.executeSql(`SELECT * FROM transactions WHERE userId = ?`, [userId],
+          (tx, results) => {
+            console.log(results.rows);
+            fetchTransactionList(results.rows)
+            // return results.rows;
+          });
+      },
+        (error) => {
+          throw new Error(JSON.stringify(error));
+        }
 
-      // );
+      );
 
     } catch (e) {
         console.error(e);
